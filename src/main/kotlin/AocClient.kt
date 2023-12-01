4b14650a8cbf809bbedcfd47697b245078b7914e
Markus Konttinen <markus@Markus.local>
2023-12-01 14:52:59 +0200

    Day 1 done

--- src/main/kotlin/AocClient.kt
+++ src/main/kotlin/AocClient.kt
@@ -13,14 +13,14 @@ object AocClient {
         throw RuntimeException("Failed to  read cookie: ${e.message}")
     }
 
-    suspend fun getAocDayInput(day: Int): String {
+    suspend fun getAocDayInput(day: Int): Array<String> {
         val file = File("inputs/$day.txt")
 
         if (file.exists()) {
-            return file.readText()
+            return getDayInputAsArray(day)
         }
 
-        val data = try {
+        try {
             HttpClient(CIO).use { client ->
                 client.get("https://adventofcode.com/2023/day/$day/input") {
                     header("Cookie", "session=$sessionId")
@@ -33,7 +33,12 @@ object AocClient {
             throw RuntimeException("Failed to fetch input for day $day: ${e.message}")
         }
 
-        return data
+        return getDayInputAsArray(day)
+    }
+
+    private fun getDayInputAsArray(day: Int): Array<String> {
+        val inputString = File("inputs/$day.txt").readText()
+        return inputString.trim().split("\n").toTypedArray()
     }
 
 }
--- src/main/kotlin/day01/day01.kt
+++ src/main/kotlin/day01/day01.kt
@@ -3,7 +3,41 @@ package day01
 object day01 {
 
     suspend fun solvePuzzle(): String {
-        AocClient.getAocDayInput(1)
-        return "1"
+        val data = AocClient.getAocDayInput(1)
+        println("Day01 part_1: " + partOne(data))
+        println("Day01 part_2: " + partTwo(data))
+        return "Done"
+    }
+
+    fun partOne(data: Array<String>): Int {
+        return data.map { row ->
+            row.filter { it.isDigit() }
+        }.map { row ->
+            val first = row.first()
+            val last = row.last()
+            "$first$last".toInt()
+        }.sum()
+    }
+
+    fun partTwo(data: Array<String>): Int {
+        return data.map { row ->
+            row.windowed(5, 1, true) {
+                when {
+                    it[0].isDigit() -> it[0].toString()
+                    it.startsWith("one") -> "1"
+                    it.startsWith("two") -> "2"
+                    it.startsWith("three") -> "3"
+                    it.startsWith("four") -> "4"
+                    it.startsWith("five") -> "5"
+                    it.startsWith("six") -> "6"
+                    it.startsWith("seven") -> "7"
+                    it.startsWith("eight") -> "8"
+                    it.startsWith("nine") -> "9"
+                    else -> ""
+                }
+            }
+        }.map {
+            it.filter { c -> c.isNotBlank() }
+        }.sumOf { (it.first() + it.last()).toInt() }
     }
 }
